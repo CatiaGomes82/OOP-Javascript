c4573af2c0e72bc52486ae0aabe863dd0e23d64d
Catia Gomes <catia_gomes82@hotmail.com>
2019-03-09 15:06:15 +0000

    add data and faux view for the application

---
+++ src/fleet-data.js
@@ -0,0 +1,114 @@
+export let fleet = [
+    {
+        "license": "QER521",
+        "type": "drone",
+        "model": "BICOL 1976",
+        "airTimeHours": 4556,
+        "base": "Brethren",
+        "latLong": "73.975969, -63.749819"
+    },
+    {
+        "license": "QER509",
+        "type": "drone",
+        "model": "QOT 1140",
+        "airTimeHours": 1945,
+        "base": "Belfair",
+        "latLong": "41.9551, -133.735312"
+    },
+    {
+        "license": "PRT751",
+        "type": "drone",
+        "model": "HOUSEDOWN 1487",
+        "airTimeHours": 3711,
+        "base": "Westboro",
+        "latLong": "36.785434, 76.357457"
+    },
+    {
+        "license": "PRT262",
+        "type": "drone",
+        "model": "STOCKPOST 1957",
+        "airTimeHours": 2028,
+        "base": "Hampstead",
+        "latLong": "-13.87049, 92.740087"
+    },
+    {
+        "license": "PRT619",
+        "type": "drone",
+        "model": "MINGA 1475",
+        "airTimeHours": 1070,
+        "base": "Boonville",
+        "latLong": "-17.35432, 3.271442"
+    },
+    {
+        "license": "QER436",
+        "type": "drone",
+        "model": "MOREGANIC 1331",
+        "airTimeHours": 4570,
+        "base": "Mappsville",
+        "latLong": "-10.023268, -134.623695"
+    },
+    {
+        "license": "QER198",
+        "type": "drone",
+        "model": "ZENOLUX 1259",
+        "airTimeHours": 2091,
+        "base": "Advance",
+        "latLong": "17.889902, -96.05478"
+    },
+    {
+        "license": "ATP288",
+        "type": "car",
+        "make": "REALMO",
+        "model": "veniam commodo ad",
+        "miles": 2478,
+        "latLong": "-20.878338, -158.48564"
+    },
+    {
+        "license": "PRT473",
+        "type": "car",
+        "make": "LEXICONDO",
+        "model": "anim ad proident",
+        "miles": 4176,
+        "latLong": "-53.696703, 176.550275"
+    },
+    {
+        "license": "ATP393",
+        "type": "car",
+        "make": "FANGOLD",
+        "model": "nostrud mollit ut",
+        "miles": 3083,
+        "latLong": "-75.159564, 87.767745"
+    },
+    {
+        "license": "QRS556",
+        "type": "car",
+        "make": "ORBEAN",
+        "model": "laborum culpa aliqua",
+        "miles": 1250,
+        "latLong": "17.067431, 138.95433"
+    },
+    {
+        "license": "PRT744",
+        "type": "car",
+        "make": "GEEKOSIS",
+        "model": "consequat labore in",
+        "miles": 1321,
+        "latLong": "-50.63193, 42.79018"
+    },
+    {
+        "license": "QRS273",
+        "type": "car",
+        "make": "PHUEL",
+        "model": "incididunt aute deserunt",
+        "miles": 3605,
+        "latLong": "-5.187349, -10.751746"
+    },
+    {
+        "license": "PRT573",
+        "type": "car",
+        "make": "HINWAY",
+        "model": "ipsum non quis",
+        "miles": 1488,
+        "latLong": "31.766257, 87.116735"
+    }
+]
